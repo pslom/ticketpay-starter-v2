40f77e1c095e22f97a597ff386f961026302471c
pslom <63178398+pslom@users.noreply.github.com>
2025-08-10 17:55:00 -0400

    Update pg-test.js

--- api/pg-test.js
+++ api/pg-test.js
@@ -1,5 +1,6 @@
+const { Pool } = require('pg');
 const pool = new Pool({
   connectionString: process.env.DATABASE_URL,
-  ssl: { rejectUnauthorized: false },
-  max: 1
+  ssl: { rejectUnauthorized: false },   // this line is required for Supabase in test
+  max: 3
 });
