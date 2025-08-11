509a5d271fcbb2b608d33c2fd2f67046d595b81f
pslom <63178398+pslom@users.noreply.github.com>
2025-08-11 09:20:23 -0400

    Update env-check.js

--- api/env-check.js
+++ api/env-check.js
@@ -1,7 +1,9 @@
-module.exports = (req, res) => {
+function handler(req, res) {
   res.setHeader('Content-Type', 'application/json');
-  res.status(200).json({
-    hasDatabaseUrl: !!process.env.DATABASE_URL,
-    hasStripeKey: !!process.env.STRIPE_SECRET_KEY
-  });
-};
+  res.status(200).end(JSON.stringify({
+    hasDatabaseUrl: Boolean(process.env.DATABASE_URL),
+    hasStripeKey: Boolean(process.env.STRIPE_SECRET_KEY),
+    node: process.version
+  }));
+}
+module.exports = handler;
