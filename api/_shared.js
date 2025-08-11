e1cb651029f64386c5e04b97510a74ad5a15c8f1
pslom <63178398+pslom@users.noreply.github.com>
2025-08-11 17:01:00 -0400

    Create _shared.js

---
+++ api/_shared.js
@@ -0,0 +1,24 @@
+// /api/_shared.js
+function cors(res, req) {
+  const allowed = (process.env.ALLOWED_ORIGIN || "").split(",").map(s => s.trim());
+  const origin = req.headers.origin;
+  if (origin && allowed.includes(origin)) {
+    res.setHeader("Access-Control-Allow-Origin", origin);
+    res.setHeader("Vary", "Origin");
+  }
+  res.setHeader("Access-Control-Allow-Methods", "GET,POST,OPTIONS");
+  res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization, X-API-Key");
+  res.setHeader("Access-Control-Allow-Credentials", "true");
+}
+
+function json(res, status, body) {
+  res.status(status).setHeader("Content-Type", "application/json");
+  res.end(JSON.stringify(body));
+}
+
+function isAuthorized(req) {
+  const key = req.headers["x-api-key"];
+  return key && key === process.env.ADMIN_API_KEY;
+}
+
+module.exports = { cors, json, isAuthorized };
