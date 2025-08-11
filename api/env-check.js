ec4c3ddfef1ad2b0923a1ea6b207e8f1eb26f8e9
pslom <63178398+pslom@users.noreply.github.com>
2025-08-11 14:51:55 -0400

    Delete api directory

--- api/env-check.js
+++
@@ -1,9 +0,0 @@
-function handler(req, res) {
-  res.setHeader('Content-Type', 'application/json');
-  res.status(200).end(JSON.stringify({
-    hasDatabaseUrl: Boolean(process.env.DATABASE_URL),
-    hasStripeKey: Boolean(process.env.STRIPE_SECRET_KEY),
-    node: process.version
-  }));
-}
-module.exports = handler;
