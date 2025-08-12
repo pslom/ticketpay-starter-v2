034bc7463034df8ac57b7876fd6bfbaa97c8a700
pslom <63178398+pslom@users.noreply.github.com>
2025-08-12 00:33:13 -0400

    Create _db.js

---
+++ api/_db.js
@@ -0,0 +1,20 @@
+// api/_db.js
+import pg from "pg";
+const { Pool } = pg;
+
+const pool = new Pool({
+  connectionString: process.env.DATABASE_URL,
+  ssl: process.env.DATABASE_URL?.includes("sslmode=require")
+    ? { rejectUnauthorized: false }
+    : undefined
+});
+
+export async function q(sql, params = []) {
+  const client = await pool.connect();
+  try {
+    const res = await client.query(sql, params);
+    return res;
+  } finally {
+    client.release();
+  }
+}
